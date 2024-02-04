0e4247016aed1c2b56b323a732083614c48e7bd9
unknown <ezequiel9604@gmail.com>
2024-02-04 17:34:01 -0500

    [ADD] for taking screenshots

---
+++ take_screenshot.ps1
@@ -0,0 +1,23 @@
+[Reflection.Assembly]::LoadWithPartialName("System.Drawing")
+function screenshot([Drawing.Rectangle]$bounds, $path) {
+   $bmp = New-Object Drawing.Bitmap $bounds.width, $bounds.height
+   $graphics = [Drawing.Graphics]::FromImage($bmp)
+
+   $graphics.CopyFromScreen($bounds.Location, [Drawing.Point]::Empty, $bounds.size)
+
+   $bmp.Save($path)
+
+   $graphics.Dispose()
+   $bmp.Dispose()
+}
+
+$bounds = [Drawing.Rectangle]::FromLTRB(0, 0, 1600, 900)
+
+$count = 0
+
+while( $count -le 10 ) {
+	screenshot $bounds "C:\Users\user\Desktop\screenshot_$count.png";
+	start-sleep -seconds 5
+	$count++;
+}
+
